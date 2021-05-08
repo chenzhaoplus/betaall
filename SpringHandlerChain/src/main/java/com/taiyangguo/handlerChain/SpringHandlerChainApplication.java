4f4e835a6ab24b68472971c9d0fe05a462535262
chenzhao <chenzhaoplus@163.com>
2021-05-08 12:49:36 +0800

    SpringHandlerChain: 责任链模式

---
+++ SpringHandlerChain/src/main/java/com/taiyangguo/handlerChain/SpringHandlerChainApplication.java
@@ -0,0 +1,13 @@
+package com.taiyangguo.handlerChain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHandlerChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHandlerChainApplication.class, args);
+	}
+
+}
---
+++ SpringHandlerChain/src/main/java/com/taiyangguo/handlerChain/controller/TestController.java
@@ -0,0 +1,25 @@
+package com.taiyangguo.handlerChain.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: cz
+ * @Date: 2021/4/23
+ * @Description:
+ */
+@RestController
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+    @GetMapping(value = "/sendMessage")
+    public String sendMessage() throws Exception {
+        log.info("[test success]");
+        return "success";
+    }
+
+}
