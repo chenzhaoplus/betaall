f9d003e69289ae9dab256552d0c83deee54c27ce
chenzhao <chenzhaoplus@163.com>
2021-06-07 14:00:39 +0800

    redisson

--- TestRedisson/src/main/java/com/taiyangguo/springtemplate/controller/TestController.java
+++ TestRedisson/src/main/java/com/taiyangguo/springtemplate/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.taiyangguo.springtemplate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,9 +19,29 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class TestController {
 
+    @Autowired
+    private RedissonClient redissonClient;
+
+    private static Integer num = 1;
+
     @GetMapping(value = "/sendMessage")
-    public String sendMessage() throws Exception {
+    public String sendMessage(String lockName, boolean clear) throws Exception {
         log.info("[test success]");
+        if(clear){
+            num = 1;
+        }
+        if (StringUtils.isEmpty(lockName)) {
+            lockName = "test";
+        }
+
+        RLock lock = redissonClient.getLock(lockName);
+        try {
+            lock.lock();
+            log.info("lock num = {}", num++);
+        } finally {
+            lock.unlock();
+        }
+
         return "success";
     }
 
